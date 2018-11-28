b463440019c3c5912128788eceba65d08568383f
jackbrr <36904529+jackbrr@users.noreply.github.com>
2018-11-28 21:31:18 +0000

    email address added for restaurant id

--- src/store.js
+++ src/store.js
@@ -1,33 +1,32 @@
-const RESTAURANT_USERS = [
-  {
-    email: 'jackbarr123@googlemail.com',
-    restaurantId: '16674842'
-  },
-  {
-    email: 'jon@jon.com',
-    restaurantId: '12831231'
-  }
+const RESTAURANT_USERS = [{
+        email: 'jackbarr123@googlemail.com',
+        restaurantId: '16674842'
+    },
+    {
+        email: 'jackbarr94@hotmail.com',
+        restaurantId: '12831231'
+    }
 ];
 
 const store = {
-  state: {
-    chosenRestaurant: {},
-    currentUser: {},
-    restaurant: false
-  },
-  setChosenRestaurant(restaurant) {
-    this.state.chosenRestaurant = restaurant;
-  },
-  updateCurrentUser(user) {
-    const restaurantUser = RESTAURANT_USERS.find(restaurantUser => restaurantUser.email === user.email);
-    if (restaurantUser) {
-      this.state.restaurant = true;
-      this.state.currentUser = { ...user, ...restaurantUser };
-      return;
-    }
+    state: {
+        chosenRestaurant: {},
+        currentUser: {},
+        restaurant: false
+    },
+    setChosenRestaurant(restaurant) {
+        this.state.chosenRestaurant = restaurant;
+    },
+    updateCurrentUser(user) {
+        const restaurantUser = RESTAURANT_USERS.find(restaurantUser => restaurantUser.email === user.email);
+        if (restaurantUser) {
+            this.state.restaurant = true;
+            this.state.currentUser = {...user, ...restaurantUser };
+            return;
+        }
 
-    this.state.currentUser = user;
-  }
+        this.state.currentUser = user;
+    }
 };
 
 export default store;
